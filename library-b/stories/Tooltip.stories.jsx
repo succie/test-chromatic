8bf840ba0e2fb553b4260c78351b6b30cbdd66fe
succie <kodai.suyama@gmail.com>
2022-01-31 04:41:21 +0900

    add prefix to storybook title

--- library-b/stories/Tooltip.stories.jsx
+++ library-b/stories/Tooltip.stories.jsx
@@ -4,7 +4,7 @@ import { Tooltip } from './Tooltip';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Tooltip',
+  title: 'library-b/Tooltip',
   component: Tooltip,
   argTypes: {}
 };
